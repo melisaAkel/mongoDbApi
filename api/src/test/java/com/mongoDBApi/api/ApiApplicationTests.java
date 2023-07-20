566c50a61b21630573d015ebadd9e92b49eababb
melisa <irem.akel@ug.bilkent.edu.tr>
2023-07-20 09:43:34 +0300

    generate

---
+++ api/src/test/java/com/mongoDBApi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongoDBApi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
